306740985cb56d7e808083826f82a99777ca29dd
admiremeforever <poddarmanish1997@gmail.com>
2023-09-25 23:12:36 +0530

    initial commit

---
+++ src/main/java/com/example/APIGuardian/ApiGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIGuardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIGuardian/ApiGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIGuardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
